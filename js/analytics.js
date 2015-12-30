e594074588cbc29fb6bc6dda97b1ded40644609c
Paul Buonopane <paul+git@earth2me.com>
2015-12-30 12:53:07 -0500

    Better analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,12 +1,12 @@
 !function(w, d) {
 	// Google Analytics
 	w['GoogleAnalyticsObject'] = 'ga';
-	w.ga = ga || function() { (ga.q = ga.q || []).push(arguments) };
+	w.ga = w.ga || function() { (ga.q = ga.q || []).push(arguments) };
 	ga('create', 'UA-71848137-1', 'auto');
 	ga('send', 'pageview');
 
 	// Quantcast
-	w._qevents = _qevents || [];
+	w._qevents = w._qevents || [];
 	_qevents.push({
 		qacct: 'p-JzjgyqRhP2AeU'
 	});
