055c7bf6d72a09e54b1b32cb133219ab0ef95620
Paul Buonopane <paul+git@earth2me.com>
2015-12-30 12:50:45 -0500

    Page metadata and better analytics

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+!function(w, d) {
+	// Google Analytics
+	w['GoogleAnalyticsObject'] = 'ga';
+	w.ga = ga || function() { (ga.q = ga.q || []).push(arguments) };
+	ga('create', 'UA-71848137-1', 'auto');
+	ga('send', 'pageview');
+
+	// Quantcast
+	w._qevents = _qevents || [];
+	_qevents.push({
+		qacct: 'p-JzjgyqRhP2AeU'
+	});
+}(window, document);
